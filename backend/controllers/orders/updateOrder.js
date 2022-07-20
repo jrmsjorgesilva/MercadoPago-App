df8e537dd0e25f9abea52724baa5fd26472623b4
jorge machado <jrms_jorgesilva@hotmail.com>
2022-07-20 18:02:50 -0300

    fix: orders route fixing bug

--- backend/controllers/orders/updateOrder.js
+++ backend/controllers/orders/updateOrder.js
@@ -1,7 +1,55 @@
-const Order = require('../../models/orderSchema');
+const Order = require("../../models/orderSchema");
 
 const updateOrder = async (req, res) => {
-    // todo
-}
+  const orderId = req.params.id;
+  const {
+    external_reference,
+    preference_id,
+    payer: { payer_id, nickname },
+    site_id,
+    items: [
+      {
+        items_id,
+        category_id,
+        currency_id,
+        description,
+        picture_url,
+        quantity,
+        unit_price,
+        title,
+      },
+    ],
+    application_id: Number,
+  } = req.body;
+  const orderToUpdate = {
+    external_reference,
+    preference_id,
+    payer: { payer_id, nickname },
+    site_id,
+    items: [
+      {
+        items_id,
+        category_id,
+        currency_id,
+        description,
+        picture_url,
+        quantity,
+        unit_price,
+        title,
+      },
+    ],
+    application_id: Number,
+  };
+
+  try {
+    const updatedOrder = await Order.updateOne({ _id: orderId }, orderToUpdate);
+    return res.status(200).json(updatedOrder);
+  } catch (error) {
+    return res.status(422).json({
+      message: `Não foi possível atualizar os dados no banco de dados para o pedido ${orderId}`,
+      error: error.message,
+    });
+  }
+};
 
 module.exports = updateOrder;
